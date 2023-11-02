9cff97a41d9bc0abe5c6ba6c5ddd90d6465e7351
이황제 <happy_kaiser@hotmail.com>
2023-11-03 02:08:09 +0900

    feat: 게임 룰에 대한 상수 생성

---
+++ src/contant/Rule.js
@@ -0,0 +1,15 @@
+export const NUMBER_RANGE = Object.freeze({
+  min: 1,
+  max: 45,
+});
+
+export const Lotto_FORM = Object.freeze({
+  range: NUMBER_RANGE,
+  length: 6,
+  price: 1000,
+});
+
+export const BONUS_BALL_FORM = Object.freeze({
+  range: NUMBER_RANGE,
+  length: 1,
+});
