c1bf7606ad9f2a6f5f4364c0f2d848f5e397692d
이황제 <happy_kaiser@hotmail.com>
2023-11-05 02:56:54 +0900

    수정

---
+++ src/contants/messages.js
@@ -0,0 +1,16 @@
+export const INPUT_MESSAGES = Object.freeze({
+  buy: "구입금액을 입력해 주세요.\n",
+});
+
+export const ERROR_MESAGES = Object.freeze({
+    isNotNumber: "[ERROR] 잘못된 형식입니다. 숫자와 ","만 사용 가능합니다.",
+    range: "[ERROR] 사용 가능한 숫자 범위는 1부터 45입니다.",
+    duplicateNumber:
+    "[ERROR] 중복된 숫자가 존재합니다.",
+    
+
+});
+
+export const RANK_MESSAGES =  Object.freeze({
+  three: "3개 일치",
+})
---
+++ src/uittils/Validate.js
@@ -0,0 +1,5 @@
+import {NUMBER_RANGE} from "../contant/Rule.js"
+
+const validateNumberRange = (number) {
+    const {min,max} = NUMBER_RANGE;
+}
