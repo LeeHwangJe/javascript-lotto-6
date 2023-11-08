87aea42fb850c73206470ea11739ba2905a6db6a
이황제 <happy_kaiser@hotmail.com>
2023-11-08 17:35:45 +0900

    feat: 랜덤 로또 번호 리스트 생성 기능

---
+++ src/PurchaseCost.js
@@ -0,0 +1,2 @@
+import Computer from "../utils/Computer.js";
+import { NUMBER } from "../utils/Constant.js";
--- src/uittils/Validate.js
+++
@@ -1,5 +0,0 @@
-import {NUMBER_RANGE} from "../contant/Rule.js"
-
-const validateNumberRange = (number) {
-    const {min,max} = NUMBER_RANGE;
-}
