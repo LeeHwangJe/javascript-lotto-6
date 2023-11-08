1aae6461a92a2c4c46ed76175ae5685deb661e79
이황제 <happy_kaiser@hotmail.com>
2023-11-08 17:28:46 +0900

    feat: 출력 메시지 비동기함수

--- View/OutputView.js
+++ View/OutputView.js
@@ -5,3 +5,15 @@ const MESSAGE = {
   OUTPUT_LOTTO_MATCH_RESULT_STATISTIC: "\n당첨 통계\n---",
   // OUTPUT_LOTTO
 };
+
+const OutputView = {
+    async outputPurchaseLottoCount(lottoCount) {
+      Console.print(`${lottoCount}개를 구매했습니다.`);
+    },
+  
+    async outputRandomLottoNumbersList(RandomLottoNumbersList) {
+      RandomLottoNumbersList.forEach((lottoList) => {
+        Console.print(`[${lottoList.join(", ")}]`);
+      });
+    },
+  
