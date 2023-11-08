c8a9196a9b7ed8930c0919c60ac1ac20a7c7da8c
이황제 <happy_kaiser@hotmail.com>
2023-11-08 17:28:07 +0900

    feat: 당첨 통계 및 수익률 출력 기능

---
+++ View/OutputView.js
@@ -0,0 +1,7 @@
+import { Console } from "@woowacourse/mission-utils";
+import { NUMBER } from "../utils/Constant.js";
+
+const MESSAGE = {
+  OUTPUT_LOTTO_MATCH_RESULT_STATISTIC: "\n당첨 통계\n---",
+  // OUTPUT_LOTTO
+};
