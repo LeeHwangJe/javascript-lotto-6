8023501929e4d9012627e483810700b8d3509f8c
이황제 <happy_kaiser@hotmail.com>
2023-11-08 16:26:30 +0900

    feat: 랜덤 로또의 일치 개수 및 검증

--- src/Lotto.js
+++ src/Lotto.js
@@ -24,18 +24,41 @@ class Lotto {
       throw new Error(ERROR.AREA_OVER_LOTTO_NUMBERS);
     } else if (this.findDuplicationInNumbers(numbers)) {
       throw new Error(ERROR.SAME_NUMBER_IN_LOTTO_NUMBERS);
-    };
+    }
   }
 
   findStringInNumbers(numbers) {
-    return numbers.some(item => isNaN(item));
+    return numbers.some((item) => isNaN(item));
   }
-  
+
   findOverNumbers(numbers) {
-    return numbers.some(item => item < NUMBER.MIN_LOTTO_NUMBER || item > NUMBER.MAX_LOTTO_NUMBER);
+    return numbers.some(
+      (item) => item < NUMBER.MIN_LOTTO_NUMBER || item > NUMBER.MAX_LOTTO_NUMBER
+    );
   }
 
   findDuplicationInNumbers(numbers) {
     const duplication_numbers = new Set(numbers);
     return duplication_numbers.size !== numbers.length;
   }
+
+  getLottoNumbers() {
+    return this.#numbers;
+  }
+
+  getLottoMathCount(lottoNumbersList) {
+    const lottoMatchCountList = [];
+    lottoNumbersList.forEach((lottoNumbers) => {
+      lottoMatchCountList.push(this.#checkLottoNumberMatch(lottoNumbers));
+    });
+    return lottoMatchCountList;
+  }
+
+  #checkLottoNumberMatch(lottoNumbers) {
+    return this.#numbers.filter((number) =>
+      lottoNumbers.includes(Number(number))
+    ).length;
+  }
+}
+
+export default Lotto;
